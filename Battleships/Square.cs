5e165b3072eef148397935bab1a6d9f0fe1cc7f1
Marcin Frankowski <marcin.frankowski@hotmail.com>
2019-08-19 19:45:25 +0200

    Square string ctor

--- Battleships/Square.cs
+++ Battleships/Square.cs
@@ -4,13 +4,33 @@ using System.Text;
 
 namespace Battleships
 {
-    public struct Square :IEquatable<Square>
+    public struct Square : IEquatable<Square>
     {
         public int Col;
         public int Row;
 
         public Square(int col, int row)
         {
+            Col = col;
+            Row = row;
+        }
+
+        public Square(string ColRow)
+        {
+            ColRow = ColRow.ToUpper();
+
+            var cols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int col = cols.IndexOf(ColRow[0]);
+            if (col < 0)
+            {
+                throw new ArgumentOutOfRangeException($"There is no column {ColRow[0]}.");
+            }
+
+            if (!Int32.TryParse(ColRow.Substring(1), out int row))
+            {
+                throw new ArgumentOutOfRangeException($"There is no row {ColRow.Substring(1)}.");
+            }
+
             Col = col;
             Row = row;
         }
