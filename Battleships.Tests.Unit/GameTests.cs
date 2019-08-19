2d02f3e17c49937ac2356b811a059181daac1b76
Marcin Frankowski <marcin.frankowski@hotmail.com>
2019-08-19 17:46:20 +0200

    Projects and empty classes

---
+++ Battleships.Tests.Unit/GameTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class GameTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
