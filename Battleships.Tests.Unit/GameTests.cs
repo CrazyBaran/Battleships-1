2428002406b87f70489a90e93adb7b69a42e8262
Marcin Frankowski <marcin.frankowski@hotmail.com>
2019-08-19 18:51:00 +0200

    Using Square struct instead of int array

--- Battleships.Tests.Unit/GameTests.cs
+++ Battleships.Tests.Unit/GameTests.cs
@@ -1,5 +1,6 @@
 using Battleships;
 using NUnit.Framework;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Tests
@@ -70,6 +71,72 @@ namespace Tests
             var invalidShips = playerBattleships.Count(l => l != 4) + npcBattleships.Count(l => l != 4);
 
             Assert.AreEqual(0, invalidShips);
+        }
+
+        [Test]
+        public void DoesCollide_WithShip_Yes()
+        {
+            Game game = new Game();
+
+            List<Ship> ships = new List<Ship>();
+            ships.Add(new Ship("RegularShip")
+            {
+                HealthySquares = new List<Square>
+                {
+                    new Square(1,1),
+                    new Square(1,2),
+                    new Square(1,3),
+                    new Square(1,4)
+                }
+            });
+
+            Ship collidingShip = new Ship("Collider")
+            {
+                HealthySquares = new List<Square>
+                {
+                    new Square(1,2),
+                    new Square(2,2),
+                    new Square(3,2),
+                    new Square(4,2)
+                }
+            };
+
+            bool doesCollide = game.DoesCollide(collidingShip, ships);
+
+            Assert.IsTrue(doesCollide);
+        }
+
+        [Test]
+        public void DoesCollide_WithShip_No()
+        {
+            Game game = new Game();
+
+            List<Ship> ships = new List<Ship>();
+            ships.Add(new Ship("RegularShip")
+            {
+                HealthySquares = new List<Square>
+                {
+                    new Square(1,1),
+                    new Square(1,2),
+                    new Square(1,3),
+                    new Square(1,4)
+                }                 
+            });
+
+            Ship notCollidingShip = new Ship("NiceShip")
+            {
+                HealthySquares = new List<Square>
+                {
+                    new Square(2,1),
+                    new Square(2,2),
+                    new Square(2,3),
+                    new Square(2,4)
+                }
+            };
+
+            bool doesCollide = game.DoesCollide(notCollidingShip, ships);
+
+            Assert.IsFalse(doesCollide);
         }
     }
 }
---
+++ Battleships/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battleships
+{
+    public struct Square :IEquatable<Square>
+    {
+        public int Col;
+        public int Row;
+
+        public Square(int col, int row)
+        {
+            Col = col;
+            Row = row;
+        }
+
+        public bool Equals(Square other)
+        {
+            return this.Col == other.Col && this.Row == other.Row;
+        }
+    }
+}
